8964f06d7f0a0349e2104c4bf56f850c600220dd
AroRaq <arkadiusz.rasz@gmail.com>
2016-12-27 12:46:17 +0100

    Postafke napraw

--- Tadzik/main.cpp
+++ Tadzik/main.cpp
@@ -4,7 +4,8 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <ctime>
-
+#include <cstdlib>
+#include <iostream>
 
 //******
 //temporary workaround
@@ -34,14 +35,13 @@ public:
 #include "levels/rpg.hpp"
 #include "levels/isayparty.hpp"
 
-#include <cstdlib>
-#include <iostream>
-
 int main(){
     srand(time(NULL));
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Tadzik", sf::Style::Default, settings);
+    sf::Image screenshot;
+    screenshot.create(1280, 720);
     window.setFramerateLimit(60);
     window.setKeyRepeatEnabled(false);
     ImGui::SFML::Init(window);
@@ -49,28 +49,34 @@ int main(){
     //sceneManager.registerScene<TREX>("TREX", &window);
     //sceneManager.registerScene<CLICKER>("CLICKER", &window);
     //sceneManager.registerScene<JUMPER>("JUMPER", &window);
-    sceneManager.registerScene<SYNTH3D>("SYNTH3D", &window);
+    //sceneManager.registerScene<SYNTH3D>("SYNTH3D", &window);
     //sceneManager.registerScene<MARIO>("MARIO", &window);
     //sceneManager.registerScene<LEVELSELECT>("LEVELSELECT", &window);
     //sceneManager.registerScene<RPG>("RPG", &window);
-    //sceneManager.registerScene<SHOOTER2D>("SHOOTER2D", &window);
+    sceneManager.registerScene<SHOOTER2D>("SHOOTER2D", &window);
     //sceneManager.registerScene<ISAYPARTY>("ISAYPARTY", &window);
+    //sceneManager.setActiveScene("TREX");
     //sceneManager.setActiveScene("JUMPER");
     //sceneManager.setActiveScene("CLICKER");
     sceneManager.setActiveScene("SYNTH3D");
     //sceneManager.setActiveScene("MARIO");
     //sceneManager.setActiveScene("LEVELSELECT");
-    //sceneManager.setActiveScene("SHOOTER2D");
+    sceneManager.setActiveScene("SHOOTER2D");
     //sceneManager.setActiveScene("RPG");
+    //sceneManager.setActiveScene("ISAYPARTY");
     sf::Clock deltaClock;
     while(window.isOpen()){
         sf::Event event;
         while(window.pollEvent(event)){
             ImGui::SFML::ProcessEvent(event);
             if(event.type == sf::Event::Closed )
-                 window.close();
+                window.close();
             if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
+            }
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::F12) {
+                screenshot = window.capture();
+                screenshot.saveToFile("screenshots/"+Utils::getDate()+".png");
             }
             if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Tilde) {
                 sceneManager.toogleCMD();
