3f8e7c5dca8507d8e1e2e6b5c22e97b665df43a6
stawrocek <stasiukoza1234@gmail.com>
2016-12-31 01:35:18 +0100

    Cube
    Uczę się od najlepszych <3

--- Tadzik/levels/arrrr.hpp
+++ Tadzik/levels/arrrr.hpp
@@ -1,7 +1,7 @@
 #ifndef ARRRR_HPP
 #define ARRRR_HPP
 
-//#include "../glad/glad.h"
+ #define GLEW_STATIC
 #include "../GL/glew.h"
 
 #include "../include/Scene.hpp"
@@ -10,6 +10,7 @@
 
 #include "../chaos/VertexArray.hpp"
 #include "../chaos/ShaderProgram.hpp"
+#include "../chaos/Transform.hpp"
 
 std::vector<GLfloat> cube_Pos = {
             -1.f, -1.f, -1.f,
@@ -65,16 +66,23 @@ public:
     ~ARRRR(){
         delete vao;
         delete sh1;
+        delete transformCube;
     }
 
     void onSceneLoadToMemory(){
-        vao = new chaos::VertexArray(3, 0, 2, 0, &cube_Pos);
-        sh1 = new chaos::ShaderProgram({std::make_pair("chaos/files/shaders/shader2.vs", GL_VERTEX_SHADER),
-                    std::make_pair("chaos/files/shaders/shader2.fs", GL_FRAGMENT_SHADER)});
+
     }
 
     void onSceneActivate(){
+        vao = new chaos::VertexArray(3, 0, 0, 0, &cube_Pos);
+        sh1 = new chaos::ShaderProgram({std::make_pair("chaos/files/shaders/shader2.vs", GL_VERTEX_SHADER),
+                    std::make_pair("chaos/files/shaders/shader2.fs", GL_FRAGMENT_SHADER)});
+        transformCube = new chaos::Transform();
 
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     }
 
     void onSceneDeactivate(){
@@ -82,16 +90,26 @@ public:
     }
 
     void draw(double deltaTime){
-        /*sh1->run();
-        sh1->setUniform("uniColor", glm::vec4(1.0f, 0.0f, 0.5f, 1.0f));
-        sh1->setUniform("mx",glm::mat4());
-        vao->bind();
-        glDrawArrays(GL_TRIANGLES, 0, vao->countVertices());
-        vao->unbind();
-        glUseProgram(0);*/
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            transformCube->moveX(-0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            transformCube->moveX(0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            transformCube->moveY(-0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            transformCube->moveY(0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+            transformCube->rotateY(0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+            transformCube->rotateY(-0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+            transformCube->rotateX(0.001*deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+            transformCube->rotateX(-0.001*deltaTime);
+
         sh1->run();
         sh1->setUniform("uniColor", glm::vec4(1.0f, 0.0f, 0.5f, 1.0f));
-        sh1->setUniform("mx",glm::mat4());
+        sh1->setUniform("mx",transformCube->getGlobalTransformMatrix());
         vao->bind();
         glDrawArrays(GL_TRIANGLES, 0, vao->countVertices());
         vao->unbind();
@@ -102,6 +120,7 @@ public:
 protected:
     chaos::VertexArray* vao = nullptr;
     chaos::ShaderProgram* sh1 = nullptr;
+    chaos::Transform* transformCube = nullptr;
 
 };
 
